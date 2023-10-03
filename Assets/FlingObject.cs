3d5e3fec008853ef0a5c03f8b4e3d9f11dfb9640
Minh Anh Ton <anhton20@gmail.com>
2023-10-03 13:51:54 -0700

    Added maxVelocity and 2ndCircle
    2nd circle is affected by the movement of player.
    
    Max velocity is set so that player don't clip out of boundaries if they are moving too fast

--- Assets/FlingObject.cs
+++ Assets/FlingObject.cs
@@ -5,6 +5,9 @@ public class FlingObject : MonoBehaviour
     private Vector3 initialPosition;
     private Vector3 mousePressPosition;
     private float dragForce = 10f;
+    public float maxVelocity = 25f;
+
+    public Rigidbody2D secondCircleRigidbody;
 
     private void OnMouseDown()
     {
@@ -13,15 +16,34 @@ public class FlingObject : MonoBehaviour
         mousePressPosition.z = 0f;
     }
 
+    private void OnMouseDrag()
+    {
+        Vector3 mouseCurrentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseCurrentPosition.z = 0f;
+
+        Vector3 dragDirection = mouseCurrentPosition - initialPosition;
+    }
+
     private void OnMouseUp()
     {
         Vector3 mouseReleasePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mouseReleasePosition.z = 0f;
 
-        Vector3 dragDirection = mouseReleasePosition - mousePressPosition;
+        Vector3 dragDirection = mouseReleasePosition - initialPosition;
         Vector3 force = dragDirection * dragForce;
 
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         rb.AddForce(force, ForceMode2D.Impulse);
+
+        // Limit the velocity of the first circle object
+        Vector2 clampedVelocity = Vector2.ClampMagnitude(rb.velocity, maxVelocity);
+        rb.velocity = clampedVelocity;
+
+        if (secondCircleRigidbody != null)
+        {
+            secondCircleRigidbody.AddForce(force, ForceMode2D.Impulse);
+            Vector2 clampedSecondCircleVelocity = Vector2.ClampMagnitude(secondCircleRigidbody.velocity, maxVelocity);
+            secondCircleRigidbody.velocity = clampedSecondCircleVelocity;
+        }
     }
 }
