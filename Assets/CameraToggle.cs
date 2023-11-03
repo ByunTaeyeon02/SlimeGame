0770367b11407524b8df47f004f3e1fba0af8b9c
Minh Anh Ton <anhton20@gmail.com>
2023-11-02 18:53:16 -0700

    betur cams with lerp

--- Assets/CameraToggle.cs
+++ Assets/CameraToggle.cs
@@ -7,7 +7,7 @@ public class CameraToggle : MonoBehaviour
 {
     public Transform target;
     public Transform targetClone;
-    public Vector3 offset = new Vector3(0f, 3f, -10f);
+    public Vector3 offset = new Vector3(2f, 3f, -10f);
     public float zoomSize = 7.0f;
     public int curCam = 3;
 
@@ -15,7 +15,12 @@ public class CameraToggle : MonoBehaviour
     public float maxSize = 15f; // Set a maximum camera size to prevent extreme zooming out
     public float distanceOffset = -2.5f;
 
-    void LateUpdate()
+    public float camSpeed = 1.5f;
+    public float maxOffsetX = 10.0f;
+    private Vector3 lastTargetPosition;
+    private Vector3 lastTargetClonePosition;
+
+    private void LateUpdate()
     {
         // Detect keyboard input to change curCam value
         if (Input.GetKeyDown(KeyCode.Alpha1))
@@ -30,35 +35,70 @@ public class CameraToggle : MonoBehaviour
         {
             curCam = 3;
         }
+    }
 
+    void FixedUpdate()
+    {
         // Follow the target with the camera based on curCam value
         if (target != null && targetClone != null)
-        {
+        {   
+            /*
+            // Calculate the direction based on position changes between frames
+            Vector3 targetVelocity = (target.position - lastTargetPosition) / Time.fixedDeltaTime;
+            Vector3 targetCloneVelocity = (targetClone.position - lastTargetClonePosition) / Time.fixedDeltaTime;
+
+            // Determine the sign of the x-component of the velocity direction
+            float xSign = 0;
+            if (targetVelocity.x > 0.5 || targetCloneVelocity.x > 0.5)
+            {
+                xSign = Mathf.Sign(targetVelocity.x);
+                if (curCam == 2)
+                {
+                    xSign = Mathf.Sign(targetCloneVelocity.x);
+                }
+            }
+
+            // Set offset.x based on the velocity direction (3 if moving forward, -3 if moving backward)
+            float xOffset = xSign > 0 ? 3f : -3f;
+
+            // Update the last positions for the next frame calculation
+            lastTargetPosition = target.position;
+            lastTargetClonePosition = targetClone.position; */
+
             if (curCam == 1)
             {
-                transform.position = new Vector3(target.position.x + offset.x,
-                                                 target.position.y + offset.y,
-                                                 offset.z);
-                Camera.main.orthographicSize = zoomSize;
+                /*
+                Vector3 targetPosition = new Vector3(target.position.x + xOffset + +offset.x,
+                                                     target.position.y + +offset.y,
+                                                     offset.z);
+
+                transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * camSpeed);
+                */
+
+                transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x + offset.x,
+                                                    target.position.y + offset.y, offset.z) , Time.deltaTime * camSpeed);
+                
+                Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, zoomSize, Time.deltaTime * camSpeed);
             }
             else if (curCam == 2)
             {
-                transform.position = new Vector3(targetClone.position.x + offset.x,
-                                                 targetClone.position.y + offset.y,
-                                                 offset.z);
-                Camera.main.orthographicSize = zoomSize;
+                transform.position = Vector3.Lerp(transform.position, new Vector3(targetClone.position.x + offset.x,
+                                                  targetClone.position.y + offset.y, offset.z), Time.deltaTime * camSpeed);
+                //Camera.main.orthographicSize = zoomSize;
+                Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, zoomSize, Time.deltaTime * camSpeed);
             }
             else
             {
                 Vector3 midpoint = (target.position + targetClone.position) / 2f;
-                transform.position = new Vector3(midpoint.x + offset.x, midpoint.y + offset.y, offset.z);
+                transform.position = Vector3.Lerp(transform.position, new Vector3(midpoint.x + offset.x, midpoint.y + offset.y, offset.z), 
+                                                                                  Time.deltaTime * camSpeed);
 
                 // Calculate the distance between the targets
                 float distance = Vector3.Distance(target.position, targetClone.position);
 
                 // Set camera size based on the distance between targets (with clamping)
                 float newSize = Mathf.Clamp(distance + distanceOffset, minSize, maxSize);
-                Camera.main.orthographicSize = newSize;
+                Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, newSize, Time.deltaTime * camSpeed);
             }
         }
     }
